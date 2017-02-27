549b9619ec35de5fc9bf84369a06dfbbd7107db9
Paul Goodfield <paul@bluegg.co.uk>
2017-02-27 15:55:55 +0000

    Remove int type hinting from methods

--- src/PaulGoodfield/BaseRepoInterface/Contracts/BaseRepositoryInterface.php
+++ src/PaulGoodfield/BaseRepoInterface/Contracts/BaseRepositoryInterface.php
@@ -6,17 +6,17 @@ interface BaseRepositoryInterface {
 
 	public function all($columns = array('*'), $order = array());
 
-	public function find(int $id, $columns = array('*'));
+	public function find($id, $columns = array('*'));
 
 	public function findWhere($fieldvals = array(), $columns = array('*'), $order = array(), $start = 0, $limit = 10000);
 
 	public function create(array $data);
 
-	public function update(int $id, array $data);
+	public function update($id, array $data);
 
-	public function delete(int $id);
+	public function delete($id);
 
-	public function attach(int $id, string $relation, int $relationId, $additional = []);
+	public function attach($id, string $relation, $relationId, $additional = []);
 
-	public function detach(int $id, string $relation, int $relationId);
+	public function detach($id, string $relation, $relationId);
 }
